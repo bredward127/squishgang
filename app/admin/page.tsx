cdf543f86fe4813834994c0217572ff2487d1171
bredward127 <3ddi300@gmail.com>
2026-07-27 18:31:31 -0400

    Silence set-state-in-effect lint rule for order fetch on mount

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -72,6 +72,7 @@ export default function AdminPage() {
 
   useEffect(() => {
     if (idToken) {
+      // eslint-disable-next-line react-hooks/set-state-in-effect
       fetchOrders(idToken);
     }
   }, [idToken]);
